b7bac94e3b5028a12cfdfe3db28c4846491caff5
NKulikova <f_19_natali@mail.ru>
2021-01-12 22:22:14 +0300

    fix

--- src/test/java/HomeWork03Test.java
+++ src/test/java/HomeWork03Test.java
@@ -32,12 +32,13 @@ public class HomeWork03Test {
 
     //локаторы
     private String filter = "fieldset[data-autotest-id=\"7893318\"]";
-    private String checkboxSamsung = "li:nth-child(8) > div > a > label > div";
+    private String checkboxSamsung = "li:nth-child(9) > div > a > label > div";
     private String checkboxXiaomi = "li:nth-child(11) > div > a > label > div";
     private String priceSort = "button[data-autotest-id=\"dprice\"]";
     private String searchResults = "div[data-zone-name=\"SearchResults\"]";
     private String articles = "article";
     private String addToCompare = "div:nth-child(2) > div:nth-child(2)";
+    private String addToCompareButton = "//article/div[4]/div[1]/h3/a/span[text()[contains(.,'%s')]]/ancestor::article/div[2]/div[2]";
 
     @BeforeAll
     public static void setUp() {
@@ -58,7 +59,11 @@ public class HomeWork03Test {
     public void yandexMarketTest() throws NoSuchElementException, InterruptedException {
         driver.get(yandexMarketUrl);
         logger.info("Открываем Яндекс.Маркет");
-        waitElement(By.linkText("Электроника")).click();
+        waitElement(By.xpath("//button/span[text()[contains(.,'Понятно')]]")).click();
+
+//        Тут поставила так, потому что для ExpectedConditions.visibilityOf не срабатывает
+        Thread.sleep(1000);
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.linkText("Электроника")))).click();
         logger.info("Переход в раздел \"Эелектроника\"");
         waitElement(By.linkText("Смартфоны")).click();
         logger.info("Переход в раздел \"Смартфоны\"");
@@ -75,54 +80,23 @@ public class HomeWork03Test {
         driver.findElement(By.cssSelector(priceSort)).click();
         logger.info("Отсортировали по цене по убыванию");
 
-        // нахожу панель результатов поиска, чтобы в ней выбрать модель для сравнени
-//        Thread.sleep(3000);
-        WebElement searchResult = waitElement(By.cssSelector(searchResults));
-        List<WebElement> searchResultsList = searchResult.findElements(By.cssSelector(articles));
-        List<WebElement> searchResultsSamsung = getProductList(searchResultsList, "Samsung");
-        List<WebElement> searchResultsXiaomi = getProductList(searchResultsList, "Xiaomi");
+        // добавление в сравнение
+        waitElement(By.xpath("//button/span[text()[contains(.,'Понятно')]]")).click();
 
-        if (searchResultsSamsung.size() != 0 && searchResultsXiaomi.size() != 0) {
-            WebElement samsungItem = searchResultsSamsung.get(0);
-            WebElement xiaomiItem = searchResultsXiaomi.get(0);
-            logger.info("Выбраны самые дешёвые Samsung и Xiaomi");
+//        Тут поставила так, потому что для ExpectedConditions.visibilityOf не срабатывает
+        Thread.sleep(1000);
+        driver.findElement(By.xpath(String.format(addToCompareButton, "Samsung"))).click();
+        driver.findElement(By.xpath(String.format(addToCompareButton, "Xiaomi"))).click();
 
-            // На следующем шаге тест падает с ошибкой:
-            // org.openqa.selenium.StaleElementReferenceException: The element reference of <article class="_1_IxNTwqll cia-vs cia-cs"> is stale;
-            // either the element is no longer attached to the DOM, it is not in the current frame context, or the document has been refreshed
-            // Но страница не обновлялась, элементы не менялись.
-            samsungItem.findElement(By.cssSelector(addToCompare));
-            logger.info("Дбавляем к сравнению Samsung");
-            xiaomiItem.findElement(By.cssSelector(addToCompare));
-            logger.info("Дбавляем к сравнению Xiaomi");
-            WebElement compare = wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.linkText("Сравнить"))));
-            compare.click();
-            logger.info("Открываем сравнение");
-            // проверить, что в сравнеии два элемента
-            List<WebElement> compareItems = driver.findElements(By.cssSelector("img ~ a"));
-            Assertions.assertEquals(compareItems.size(), 2);
-        }
-        else logger.info("Нет результатов для сравнения");
+        WebElement compare = wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.linkText("Сравнить"))));
+        compare.click();
+        logger.info("Открываем сравнение");
+        // проверить, что в сравнеии два элемента
+        List<WebElement> compareItems = driver.findElements(By.cssSelector("img ~ a"));
+        Assertions.assertEquals(compareItems.size(), 2);
     }
 
     private WebElement waitElement(By by) {
         return wait.until(ExpectedConditions.visibilityOf(driver.findElement(by)));
-    }
-
-    private List<WebElement> waitElements(By by) {
-        return wait.until(ExpectedConditions.visibilityOfAllElements(driver.findElements(by)));
-    }
-
-    private List<WebElement> getProductList(List<WebElement> commonList, String name) {
-        List<WebElement> resultList = new ArrayList<WebElement>();
-        if (commonList.size() != 0) {
-            for (int i = 0; resultList.size() < 1; i++) {
-                if (commonList.get(i).findElement(By.cssSelector("h3 > a > span")).getText().contains(name)) {
-                    resultList.add(commonList.get(i));
-                    continue;
-                }
-            }
-        }
-        return resultList;
     }
 }
